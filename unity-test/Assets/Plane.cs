b580d5a0108abf970d4158aa70ac93d17501bde9
Maciej Rosiek <maciej.rosiek@gmail.com>
2015-10-10 11:05:53 +0200

    Added unity test project

---
+++ unity-test/Assets/Plane.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class Plane : MonoBehaviour {
+
+    static float _deadzone = 0.1f;
+    Animator animator;
+    int speed = 6;
+    public GameObject plane;
+    int moveDirection = 0;
+
+	void Start () {
+        animator = GetComponent<Animator>();
+	}
+
+    private int lastMousePosition = 0;
+
+    void Update()
+    {
+        float axisInput = Input.GetAxisRaw("Horizontal");
+        moveDirection = (int) axisInput;
+        if (axisInput == 0)
+        {
+            if (Input.GetButton("Fire1"))
+            {
+               moveDirection = Math.Sign(Input.mousePosition.x - lastMousePosition);     
+            }
+            if (Input.GetButtonUp("Fire1"))
+            {
+                lastMousePosition = 0;
+            }
+        }
+        animator.SetInteger("speed", - Math.Sign(moveDirection));
+        if (moveDirection > 0 && transform.position.x < 3.5)
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+        if (moveDirection < 0 && transform.position.x > -3.5)
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+
+        lastMousePosition = (int)Input.mousePosition.x;
+    }
+}
